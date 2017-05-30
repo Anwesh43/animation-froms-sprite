bab1965f337eb220aa5b6bbc1957908ddc96c9f6
Anwesh43 <anweshthecool0@gmail.com>
2017-05-30 18:53:38 +0530

    adding spriteanimator

---
+++ animation-from-sprite.js
@@ -0,0 +1,54 @@
+class SpriteAnimator extends HTMLElement{
+    constructor(src) {
+        super()
+        this.src = this.getAttribute('src')
+        this.rows = this.getAttribute('rows') || 0
+        this.cols = this.getAttribute('cols') || 0
+        this.img = document.createElement('img')
+        this.delay = parseInt()
+        this.state = {x:0,y:0}
+    }
+    setState(obj) {
+        this.state = Object.assign({},this.state,obj)
+    }
+    render() {
+        const w = this.state.w, h = this.state.h
+        var spriteW = w ,spriteH = h,x = this.state.x,y = this.state.y
+        if(this.rows != 0) {
+            spriteH = h/(this.rows)
+        }
+        if(this.cols != 0) {
+            spriteW=w/this.cols
+        }
+        const canvas = document.createElement('canvas')
+        canvas.width = spriteW
+        canvas.height = spriteH
+        const context = canvas.getContext('2d')
+        context.clearRect(0,0,spriteW,spriteH)
+        context.save()
+        context.translate(-x,-y)
+        context.drawImage(this.image,0,0)
+        context.restore()
+        x += spriteW
+        if(x > = w) {
+            x = 0
+            y += spriteH
+            if(y >= h) {
+                y = 0
+            }
+        }
+        this.setState({x,y})
+        this.img.src = canvas.toDataURL()
+    }
+    connectedCallback() {
+        this.image = new Image()
+        this.image.src = this.src
+        this.image.onload = () => {
+            this.setState({w:this.image.width,h:this.image.height})
+            const interval = setInterval(()=>{
+                this.render()
+            },this.delay)
+        }
+    }
+}
+customElements.define('sprite-animator',SpriteAnimator)
