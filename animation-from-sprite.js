f8e698638db95aaef9c68cf6583947d479e9c5f0
Anwesh43 <anweshthecool0@gmail.com>
2017-05-30 19:20:35 +0530

    using xoffset and yoffset

--- animation-from-sprite.js
+++ animation-from-sprite.js
@@ -4,11 +4,15 @@ class SpriteAnimator extends HTMLElement{
         this.src = this.getAttribute('src')
         this.rows = this.getAttribute('rows') || 0
         this.cols = this.getAttribute('cols') || 0
+        this.xoffset = this.getAttribute('xoffset') || 0
+        this.yoffset = this.getAttribute('yoffset') || 0
         this.img = document.createElement('img')
         this.delay = this.getAttribute('delay') || 100
         this.state = {x:0,y:0}
         const shadow = this.attachShadow({mode:'open'})
         shadow.appendChild(this.img)
+        console.log(this.xoffset)
+        console.log(this.yoffset)
     }
     setState(obj) {
         this.state = Object.assign({},this.state,obj)
@@ -23,20 +27,22 @@ class SpriteAnimator extends HTMLElement{
             spriteW=w/this.cols
         }
         const canvas = document.createElement('canvas')
-        canvas.width = spriteW
-        canvas.height = spriteH
+        canvas.width = spriteW*(1-this.xoffset)
+        canvas.height = spriteH*(1-this.yoffset)
         const context = canvas.getContext('2d')
-        context.clearRect(0,0,spriteW,spriteH)
+        context.clearRect(0,0,spriteW*(1-this.xoffset),spriteH*(1-this.yoffset))
         context.save()
         context.translate(-x,-y)
         context.drawImage(this.image,0,0)
         context.restore()
         x += spriteW
         if(x >= w) {
+
             x = 0
             y += spriteH
             if(y >= h) {
                 y = 0
+                console.log("coming here"+this.src)
             }
         }
         this.setState({x,y})
